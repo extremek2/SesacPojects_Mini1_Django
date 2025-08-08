bb61ffd7a3149e5ad1d01e347619cba8875a62d0
honggildong <hong@sasac.com>
2025-08-08 17:51:42 +0900

    수정내용

--- cart/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import Cart
-
-def cart_item_count(request):
-    if request.user.is_authenticated:
-        cart, created = Cart.objects.get_or_create(user=request.user)
-        return {'cart_item_count': sum(item.quantity for item in cart.cartitem_set.all())}
-    return {'cart_item_count': 0}
--- ourproject/settings.py
+++ ourproject/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'single_pages',
-    'cart',
-    'product',
+    
 ]
 
 MIDDLEWARE = [
@@ -65,7 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processors.cart_item_count',
             ],
         },
     },
