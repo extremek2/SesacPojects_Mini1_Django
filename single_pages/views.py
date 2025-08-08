60f92e9103623d18684c935256a494de738a12f1
extremek <extremek@naver.com>
2025-08-08 13:55:03 +0900

    템플릿 초기 세팅

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+def index(request):
+    return render(request, 'single_pages/index.html')
+
+def starter(request):
+    return render(request, 'single_pages/starter-page.html')
 # Create your views here.
