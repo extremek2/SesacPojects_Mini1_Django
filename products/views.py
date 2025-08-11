9b03f6a26add9e10fbb915250f7ca06d73659869
extremek <extremek@naver.com>
2025-08-11 21:47:50 +0900

    products 템플릿 임시 교체 및 테스트

--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import SeasonalProducts, Products
+from .models import SeasonalProducts, Products, Category
 
 def products_index(request):
     products = Products.objects.all()
+    categories = Category.objects.all()
     seasonal_products = SeasonalProducts.objects.all()
-    return render(request, 'products/index.html',
+    return render(request, 'products/foody2_product.html',
                   context={'products': products,
-                           'seasonal_products': seasonal_products})
+                           'seasonal_products': seasonal_products,
+                           'categories': categories})
 
 def detail(request, pk):
     product = Products.objects.get(pk=pk)
@@ -14,3 +16,15 @@ def detail(request, pk):
     return render(request,
                   template_name='products/product_detail.html',
                   context={'product': product})
+
+def category(request, slug):
+    categories = Category.objects.all()
+    if slug == "no_category":
+        products = Products.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        products = Products.objects.filter(category=category)
+    return render(request,
+                  template_name='products/index.html',
+                  context={'products': products, 'categories': categories,
+                           'category': category})
