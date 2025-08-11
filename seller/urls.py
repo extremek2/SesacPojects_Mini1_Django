0ff712f29ae3e4b0e291990bd13272ef28273bb4
hong <hong@sesac.com>
2025-08-11 13:50:39 +0900

    seller로그인 회원가입 대시보드 생성

--- seller/urls.py
+++ seller/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.seller_login,name='seller_login'),
-
-
+    path('seller_signup/',views.seller_signup,name='seller_signup'),
+    path('seller_login/',views.seller_login,name='seller_login'),
+    path('seller_logout/', views.seller_logout, name='seller_logout'),
+    path('seller_dashboard/', views.seller_dashboard, name='seller_dashboard'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import Seller
 
 # Create your views here.
+#판매자 로그인
 def seller_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            seller = Seller.objects.get(username=username)
+            if seller.password == password:
+                request.session['seller_id'] = seller.id
+                return redirect('seller_dashboard')
+            else:
+                messages.error(request, '비밀번호가 틀렸습니다')
+        except Seller.DoesNotExist:
+            messages.error(request, '존재하지 않는 판매자입니다')
+
     return render(request, 'seller/seller_login.html')
+
+#판매자 회원가입
+def seller_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        if password != password2:
+            messages.error(request, '비밀번호가 일치하지 않습니다')
+            return redirect('seller_signup')
+
+        if Seller.objects.filter(username=username).exists():
+            messages.error(request, '이미 존재하는 판매자 아이디입니다')
+            return redirect('seller_signup')
+
+        Seller.objects.create(username=username, password=password)
+        return redirect('seller_login')
+
+    return render(request, 'seller/seller_signup.html')
+
+#판매자 상세페이지용
+def seller_dashboard(request):
+    seller_id = request.session.get('seller_id')
+    if not seller_id:
+        return redirect('seller_login')
+
+    seller = Seller.objects.get(id=seller_id)
+    return render(request, 'seller/dashboard.html', {'seller': seller})
+
+#f판매자 로그아웃
+def seller_logout(request):
+    # 세션에서 seller_id 삭제
+    if 'seller_id' in request.session:
+        del request.session['seller_id']
+    return redirect('seller_login')
