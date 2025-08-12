5e2f06eef0344eb7f61834d9823c6ba64fe171e9
honggildong <hong@sasac.com>
2025-08-12 11:15:08 +0900

    0812-2

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.products_index,name='products_index'),
-    path('<int:pk>/',views.detail,name='products_detail'),
 
 
 ]
--- recipes/models.py
+++ recipes/models.py
@@ -79,4 +79,3 @@ class RecipeCourse(models.Model):
     step_tip = models.CharField(max_length=100)
     def __str__(self):
         return(f'{self.recipe_id}-{self.cooking_no}-{self.cooking_dc}-{self.step_tip}')
-
