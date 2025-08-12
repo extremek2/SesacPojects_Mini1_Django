8000f0eee5b82197b376407a087de0365eb7a977
honggildong <hong@sasac.com>
2025-08-12 11:47:30 +0900

    0812-덮어쓰기

---
+++ ourproject/ourproject/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for ourproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('main/', include('single_pages.urls')),
+    # path('accounts/', include('allauth.urls')),구글어서용 패스 일단 주석처리해놓음
+    path('users/', include('users.urls')),#로그인시 users앱 url로 이동
+
+    path('', include('single_pages.urls')),
+    path('products/', include('products.urls')),
+    path('recipes/', include('recipes.urls')),
+    path('seller/', include('seller.urls')),
+
+]
